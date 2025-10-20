bdfc4d63d67c5ff0ac4f41ffd75a0f21d054c175
yhwHUB <yanghw1902@gmail.com>
2025-10-20 15:35:41 +0900

    feat(community) : 댓글 기능 작성 중

--- backend/src/main/java/com/backend/domain/community/service/CommunityService.java
+++ backend/src/main/java/com/backend/domain/community/service/CommunityService.java
@@ -1,5 +1,6 @@
 package com.backend.domain.community.service;
 
+import com.backend.domain.community.entity.Comment;
 import com.backend.domain.repository.entity.Repositories;
 import com.backend.domain.repository.repository.RepositoryJpaRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,5 +19,12 @@ public class CommunityService {
     // publicRepository(repository 필드)가 true인 리포지토리 조회
     public List<Repositories> getRepositoriesPublicTrue(){
         return repositoryJpaRepository.findByPublicRepository(true);
+    }
+
+    // 리포지토리에 댓글 작성
+    public Comment addComment(String content){
+        Comment comment = new Comment(content, this);
+        this.comments.add(comment);
+        return comment;
     }
 }
