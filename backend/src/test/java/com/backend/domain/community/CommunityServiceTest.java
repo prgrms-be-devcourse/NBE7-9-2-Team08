f133912113685ee826f6aeaaded0f4835e97b417
yhwHUB <yanghw1902@gmail.com>
2025-11-08 01:56:58 +0900

    test(be/community) : addComment 테스트 코드 작성

--- backend/src/test/java/com/backend/domain/community/CommunityServiceTest.java
+++ backend/src/test/java/com/backend/domain/community/CommunityServiceTest.java
@@ -1,6 +1,7 @@
 package com.backend.domain.community;
 
 import com.backend.domain.analysis.entity.AnalysisResult;
+import com.backend.domain.analysis.repository.AnalysisResultRepository;
 import com.backend.domain.community.entity.Comment;
 import com.backend.domain.community.repository.CommentRepository;
 import com.backend.domain.community.service.CommunityService;
@@ -14,8 +15,10 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -26,6 +29,9 @@ public class CommunityServiceTest {
 
     @Mock
     private CommentRepository commentRepository;
+
+    @Mock
+    private AnalysisResultRepository analysisResultRepository;
 
     @InjectMocks
     private CommunityService communityService;
@@ -98,5 +104,39 @@ public class CommunityServiceTest {
 
         verify(commentRepository, times(1))
                 .findByAnalysisResultIdOrderByIdDesc(1L);
+    }
+
+    @Test
+    @DisplayName("댓글 작성 테스트")
+    void writeComment(){
+        // 작성 한 내용
+        AnalysisResult analysisResult1 = AnalysisResult.builder()
+                .id(1L)
+                .build();
+
+        when(analysisResultRepository.findById(1L))
+                .thenReturn(Optional.of(analysisResult1)); // findById는 Optional이라서 null / 결과를 반환 -> 여기선 확실하ㅔㄱ null을 반환하니까 테스트용 데이터를 반환해라
+
+        Comment comment1 = Comment.builder()
+                .id(1L)
+                .analysisResult(analysisResult1)
+                .memberId(1L)
+                .comment("write Test 1")
+                .build();
+
+        when(commentRepository.save(any(Comment.class)))
+                .thenReturn(comment1);
+
+        // 작성
+        Comment writeResult = communityService.addComment(1L, 1L, "write Test 1");
+
+        // 작성 후 내용이 작성 했던 내용이 그대로 들어왔는지 확인
+        assertNotNull(writeResult);
+        assertEquals(1L, writeResult.getMemberId());
+        assertEquals("write Test 1", writeResult.getComment());
+        assertEquals(1L, writeResult.getAnalysisResult().getId());
+
+        verify(analysisResultRepository, times(1)).findById(1L);
+        verify(commentRepository, times(1)).save(any(Comment.class));
     }
 }
