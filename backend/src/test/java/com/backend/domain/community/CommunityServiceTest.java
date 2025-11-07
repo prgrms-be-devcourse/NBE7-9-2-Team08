ff9896272966d9a1d5c5a79d3d7bedb82f23b4bf
yhwHUB <yanghw1902@gmail.com>
2025-11-08 01:04:21 +0900

    feat(be/community) : CommunityServiceTest 조회 부분 작성

---
+++ backend/src/test/java/com/backend/domain/community/CommunityServiceTest.java
@@ -0,0 +1,102 @@
+package com.backend.domain.community;
+
+import com.backend.domain.analysis.entity.AnalysisResult;
+import com.backend.domain.community.entity.Comment;
+import com.backend.domain.community.repository.CommentRepository;
+import com.backend.domain.community.service.CommunityService;
+import com.backend.domain.repository.entity.Repositories;
+import com.backend.domain.repository.repository.RepositoryJpaRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class CommunityServiceTest {
+
+    @Mock
+    private RepositoryJpaRepository repositoryJpaRepository;
+
+    @Mock
+    private CommentRepository commentRepository;
+
+    @InjectMocks
+    private CommunityService communityService;
+
+    @Test
+    @DisplayName("커뮤니티 분석결과 조회 테스트")
+    void getCommunityRepositoryList(){
+
+        // 테스트 데이터 만들기
+        Repositories repo1 = Repositories.builder()
+                .name("Repo1")
+                .htmlUrl("url1")
+                .publicRepository(true)
+                .build();
+
+        Repositories repo2 = Repositories.builder()
+                .name("Repo2")
+                .htmlUrl("url2")
+                .publicRepository(true)
+                .build();
+
+        // 리포지토리에서 함수를 호출할 때 해당 결과로 반환하도록 하기
+        when(repositoryJpaRepository.findByPublicRepository(true))
+                .thenReturn(List.of(repo1, repo2));
+
+
+        // Service 코드로 조회 하기
+        // NPE 발생 :
+        List<Repositories> result = communityService.getRepositoriesPublicTrue();
+
+        // 호출한 내용과 위 데이터 내용이 같은지 확인
+        assertEquals(2, result.size());
+        assertEquals("Repo1", result.get(0).getName());
+        assertEquals("Repo2", result.get(1).getName());
+
+        // repository가 정확한지 확인
+        verify(repositoryJpaRepository, times(1)).findByPublicRepository(true);
+    }
+
+    @Test
+    @DisplayName("댓글 조회 테스트")
+    void getCommnets(){
+        // 테스트 데이터 만들기
+        AnalysisResult analysisResult1 = AnalysisResult.builder()
+                .id(1L)
+                .build();
+
+        Comment comment1 = Comment.builder()
+                .id(1L)
+                .analysisResult(analysisResult1) // 가짜 analysisResult 넣어주기
+                .memberId(1L)
+                .comment("test comment 1")
+                .build();
+
+        Comment comment2 = Comment.builder()
+                .id(2L)
+                .memberId(1L)
+                .comment("test comment 2")
+                .build();
+
+        // 리포지토리에서 서비스 함수를 호출할 때 테스트 데이터를 반환하도록 하기
+        when(commentRepository.findByAnalysisResultIdOrderByIdDesc(1L))
+                .thenReturn(List.of(comment1, comment2));
+
+        // 서비스 코드로 데이터 조회
+        List<Comment> result = communityService.getCommentsByAnalysisResult(1L);
+
+        assertEquals(1, result.size());
+        assertEquals("test comment 1", result.get(0).getComment());
+
+        verify(commentRepository, times(1))
+                .findByAnalysisResultIdOrderByIdDesc(1L);
+    }
+}
